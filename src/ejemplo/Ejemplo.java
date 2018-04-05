2a510934e36c30f80bfee9161860bf40d668db6c
Juan Londono <Juan Londono@JuanLondono>
2018-04-04 21:30:35 -0500

    Se borraron comentarios Se agrego una variable numero

--- src/ejemplo/Ejemplo.java
+++ src/ejemplo/Ejemplo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejemplo;
 
 /**
@@ -15,7 +10,10 @@ public class Ejemplo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int numero;
+        numero = 1;
+        System.out.println("El numero es: " + 1);
     }
     
 }
